8c2ab1ab5d48efe6c547d0456251d50ea4dc120f
Arturo Jamaica <me@arturojamaica.com>
2021-05-18 09:53:25 +0900

    Make ui for launch screen

--- Summer/Views/StartView/UI/StartViewController.swift
+++ Summer/Views/StartView/UI/StartViewController.swift
@@ -20,5 +20,10 @@ class StartViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+    }
 
 }
